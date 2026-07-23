18a3b83fd8dea8d74da82c8e9e196bf82292a771
Andy-Hsu0127 <Andy-Hsu0127@users.noreply.github.com>
2026-07-23 15:27:37 +0800

    feat(control): hide cmd window on launch, browser-only UI

--- control.bat
+++ control.bat
@@ -1,6 +1,10 @@
 @echo off
+if "%1"=="hidden" goto run
+powershell -WindowStyle Hidden -Command "Start-Process cmd -ArgumentList '/c \"%~f0\" hidden' -WindowStyle Hidden"
+exit /b
+
+:run
 node tools/gui-controller/controller.js
-pause
 
 
 
